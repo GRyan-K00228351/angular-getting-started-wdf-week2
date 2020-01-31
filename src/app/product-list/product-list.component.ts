148bf8e264d64d2d2013fe67f068630b95aba8c0
GRyan-K00228351 <k00228351@student.lit.ie>
2020-01-31 10:01:03 +0000

    Changed products array variable name to shopProducts for clarity

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -9,7 +9,7 @@ import { products } from '../products';
 })
 export class ProductListComponent {
   message:String = "Hello, Salut";
-  products = products;
+  shopProducts = products;
 
   share() {
     window.alert('The product has been shared!');
