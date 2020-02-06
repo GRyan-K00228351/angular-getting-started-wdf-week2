6c5dc0cfe5cadaf1a0d5348433901b5a122b43a7
GRyan-K00228351 <k00228351@student.lit.ie>
2020-02-06 19:30:24 +0000

    Importing and injecting the cart service

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -12,6 +13,7 @@ export class ProductDetailsComponent implements OnInit {
  public product:any; //decalring a public variable of type any
   constructor( 
     private route: ActivatedRoute,
+    private cartService: CartSer
   ) { }
 
   ngOnInit() {
